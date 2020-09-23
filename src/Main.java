21a2e8d0bdfb016aa7e45500be639d5da3e57646
Rikardo <rikardo@marenzzi.de>
2020-09-23 11:44:51 +0200

    Variabel Namen geändert

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,60 @@
 import java.util.Scanner;
 
+
 public class Main {
 
     /*
     Hier sollte ein wichtiges Kommentar stehen :)
      */
 
-    public static void main(String[] args) {
-
-        Scanner eingabewert = new Scanner(System.in);
-
-        System.out.println("Bitte Vorname des Benutzers eingeben: ");
-        String vorname = eingabewert.nextLine();
-
-        System.out.println("Bitte Nachname des Benutzers eingeben: ");
-        String nachname = eingabewert.nextLine();
-
-        System.out.println("Bitte Alter des Benutzeres eingeben: ");
-        int Alter = eingabewert.nextInt();
-
-        System.out.println("Der Benutzer " + vorname + " " + nachname + " ist aktuell " + Alter + " Jahre alt und würde gerne in Zukunft eine Achterbahn benutzen.");
-        System.out.println("Die Achterbahn darf nur im Alter zwischen 16 und 60 Jahren genutzt werden.");
-        System.out.println("Darf der Benutzer in ... Jahren die Achterbahn (noch) benutzen?");
-        System.out.println("Bitte Jahr eingeben: ");
-        int Jahr = eingabewert.nextInt();
-        Alter = Alter +Jahr;
-        System.out.println("----");
-        System.out.println("In " + Jahr + " Jahren wird der Benutzer " + vorname + " " + nachname + " " + Alter + " Jahre alt sein." );
-
-        if (Alter > 16 && Alter < 60) {
-            System.out.println("Der Benutzer " + vorname + " " + nachname + " darf die Achterbahn benutzen.");
-        } else {
-            System.out.println("Der Benutzer " + vorname + " " + nachname + " darf die Achterbahn NICHT benutzen.");
-        }
-
-
-
-
+    public static int sum(int value_age, int value_year) {
+        return value_age + value_year;
     }
 
 
+    public static String check_age(int value_age, String value_name, String value_surname) {
+
+        String message;
+
+        if (value_age >= 16 && value_age <= 60) {
+                message = "Der Benutzer " + value_name + " " + value_surname + " darf die Achterbahn benutzen.";
+            } else {
+                message = "Der Benutzer " + value_name + " " + value_surname + " darf die Achterbahn NICHT benutzen.";
+            }
+
+        return message;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Scanner input_value = new Scanner(System.in);
+
+        System.out.println("Bitte Vorname des Benutzers eingeben: ");
+        String name = input_value.nextLine();
+
+        System.out.println("Bitte Nachname des Benutzers eingeben: ");
+        String surname = input_value.nextLine();
+
+        System.out.println("Bitte Alter des Benutzers eingeben: ");
+        int age = input_value.nextInt();
+
+        System.out.println("Der Benutzer " + name + " " + surname + " ist aktuell " + age + " Jahre alt und würde gerne in Zukunft eine Achterbahn benutzen.");
+        System.out.println("Die Achterbahn darf nur im Alter zwischen 16 und 60 Jahren genutzt werden.");
+        System.out.println("Darf der Benutzer in ... Jahren die Achterbahn (noch) benutzen?");
+        System.out.println("Bitte Jahr eingeben: ");
+
+        int Jahr = input_value.nextInt();
+        age = sum(age,Jahr);
+
+        System.out.println("----");
+        System.out.println("In " + Jahr + " Jahren wird der Benutzer " + name + " " + surname + " " + age + " Jahre alt sein." );
+
+        System.out.println(check_age(age,name,surname));
+
+    }
+
 }
+
+
