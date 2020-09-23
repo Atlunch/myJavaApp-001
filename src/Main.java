1955abf594bcec01180514d9d1fbb113ada432eb
Rikardo <rikardo@marenzzi.de>
2020-09-23 10:57:19 +0200

    If Abfrage zum Alter und Benutzung der Achterbahn

--- src/Main.java
+++ src/Main.java
@@ -16,15 +16,25 @@ public class Main {
         System.out.println("Bitte Nachname des Benutzers eingeben: ");
         String nachname = eingabewert.nextLine();
 
-        int Alter = 28;
+        System.out.println("Bitte Alter des Benutzeres eingeben: ");
+        int Alter = eingabewert.nextInt();
 
-        System.out.println("Der Benutzer " + vorname + " " + nachname + " ist aktuell " + Alter + " Jahre alt.");
-        System.out.println("Wie Alt wird der Benutzer in ... Jahren sein?");
+        System.out.println("Der Benutzer " + vorname + " " + nachname + " ist aktuell " + Alter + " Jahre alt und würde gerne in Zukunft eine Achterbahn benutzen.");
+        System.out.println("Die Achterbahn darf nur im Alter zwischen 16 und 60 Jahren genutzt werden.");
+        System.out.println("Darf der Benutzer in ... Jahren die Achterbahn (noch) benutzen?");
         System.out.println("Bitte Jahr eingeben: ");
         int Jahr = eingabewert.nextInt();
         Alter = Alter +Jahr;
         System.out.println("----");
         System.out.println("In " + Jahr + " Jahren wird der Benutzer " + vorname + " " + nachname + " " + Alter + " Jahre alt sein." );
+
+        if (Alter > 16 && Alter < 60) {
+            System.out.println("Der Benutzer " + vorname + " " + nachname + " darf die Achterbahn benutzen.");
+        } else {
+            System.out.println("Der Benutzer " + vorname + " " + nachname + " darf die Achterbahn NICHT benutzen.");
+        }
+
+
 
 
     }
